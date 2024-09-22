e11618c0a81912b86fd520e441d346ec837ee708
Alal Hosen <alalhosen1992620120@gmail.com>
2024-09-22 23:34:53 +0600

    add Home Component

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
     return (
         <div>
-            <h2>This is the home component</h2>
+            <h1>I want a Home Component.</h1>
+            <h2>This is the home component.</h2>
+            <h2>Home component is a best component.</h2>
+
         </div>
     );
 };
