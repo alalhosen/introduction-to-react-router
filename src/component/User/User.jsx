cc69c8b2f717232c44cbc0f7c2bb435f7b46e8df
Alal Hosen <alalhosen1992620120@gmail.com>
2024-09-27 22:33:06 +0600

    create users.css file and use css in users

--- src/component/User/User.jsx
+++ src/component/User/User.jsx
@@ -1,8 +1,13 @@
 
 const User = ({ user }) => {
     const { id, name, email, phone } = user;
+    const userStyle = {
+        border: '3px solid yellow',
+        padding: '10px',
+        borderRadius: '20px'
+    }
     return (
-        <div>
+        <div style={userStyle}>
             <h2>{name}</h2>
             <p>email:{email}</p>
             <p>phone:{phone}</p>
--- src/component/Users/Users.jsx
+++ src/component/Users/Users.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import User from "../User/User";
+import './Users.css';
 
 const Users = () => {
 
@@ -14,7 +15,7 @@ const Users = () => {
         <div>
             <h3>Our Users:{users.length}</h3>
             <p>Our users are best users.</p>
-            <div>
+            <div className="users">
                {
                 users.map(user =><User key={user.id} user={user}></User>)
                }
