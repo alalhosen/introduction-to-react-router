1d4e7d68fbce82003af69bf88d9bf7bd3fe7485f
Alal Hosen <alalhosen1992620120@gmail.com>
2024-09-28 23:57:18 +0600

    set map of post file

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -1,6 +1,11 @@
 
 const Post = ({post}) => {
     const {id, title}=post;
+    const userStyle = {
+        border: '3px solid yellow',
+        padding: '10px',
+        borderRadius: '20px'
+    }
     return (
         <div>
             <h4>Post of Id:{id}</h4>
