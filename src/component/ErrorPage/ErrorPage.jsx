5687a68fd933ddb67b09ebdd5db37cf03a09135e
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-02 23:32:06 +0600

    active link style setting

--- src/component/ErrorPage/ErrorPage.jsx
+++ src/component/ErrorPage/ErrorPage.jsx
@@ -16,7 +16,7 @@ const ErrorPage = () => {
                 </div>
             }
         </div>
-    );
+    );  
 };
 
 export default ErrorPage;
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
@@ -6,8 +6,8 @@ const Header = () => {
             <nav>
                 <span>My website</span>
                 <Link to="/">Home</Link>
-                <Link to="users">Users</Link>
-                <Link to="/posts">Posts</Link>
+                <NavLink to="users">Users</NavLink>
+               <NavLink to="/posts">Posts</NavLink>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact</Link>
             </nav>
